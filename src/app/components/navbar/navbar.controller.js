febc7766b54452547581298e2e3ce25b15ea5755
Jimmy King <jimmy.king@meltmedia.com>
2015-04-24 11:20:42 -0700

    yo gulp-angular

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('totemDashboard')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('totemDashboard', ['ngAnimate', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
