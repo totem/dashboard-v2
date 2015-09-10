a2df1c0f2d7252769ddaef0fcf47836c7b570ef1
Jimmy King <jimmy.king@meltmedia.com>
2015-09-10 10:51:47 -0700

    Started on clone functionality

--- app/components/applications/selected/applications-selected-controller.js
+++ app/components/applications/selected/applications-selected-controller.js
@@ -276,6 +276,13 @@ angular.module('totemDashboard')
     });
   };
 
+  $scope.cloneDeployment = function (deployment) {
+    $scope.working = true;
+    api.cloneDeployment(deployment).then(function () {
+      $scope.working = false;
+    });
+  };
+
   $scope.deleteDeployment = function (deployment) {
     $scope.working = true;
     api.deleteDeployment(deployment.deployment.name, deployment.metaInfo.deployer.url).then(function () {
